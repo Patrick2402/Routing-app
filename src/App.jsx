975c6607a26c7b34961d94b94d8281624eabd068
Patryk Zawieja <patryk.zawieja24@gmail.com>
2023-01-05 23:06:40 +0100

    Lesson 2 Router Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import { useState } from 'react'
-import { Route, Routes, BrowserRouter, Link, NavLink } from 'react-router-dom';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom';
 import './App.css'
 
 // Paths
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
-import Navbar from './Pages/Navbar';
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+)
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <Navbar />
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path='contact' element={<Contact />}/>
-        </Routes>
-      </main>
-    </BrowserRouter>
+  <RouterProvider router={router} />
   )
 }
 
